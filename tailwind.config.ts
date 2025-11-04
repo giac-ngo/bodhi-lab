68b09b55f3fb7cf87f698fa39d8b86ba1efbf64a
bankericc <49155682-bankericc@users.noreply.replit.com>
2025-11-04 08:23:22 +0000

    Restored to '2fbc45d6cfa4245d7935bf69707e67f07df0a84d'
    Replit-Restored-To: 2fbc45d6cfa4245d7935bf69707e67f07df0a84d

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,10 +2,7 @@ import type { Config } from "tailwindcss";
 
 export default {
   darkMode: ["class"],
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./client/index.html", "./client/src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       borderRadius: {
@@ -14,6 +11,7 @@ export default {
         sm: ".1875rem", /* 3px */
       },
       colors: {
+        // Flat / base colors (regular buttons)
         background: "hsl(var(--background) / <alpha-value>)",
         foreground: "hsl(var(--foreground) / <alpha-value>)",
         border: "hsl(var(--border) / <alpha-value>)",
